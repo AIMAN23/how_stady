401cc930851296d95f53f98fea47617affff2145
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-05-20 02:53:39 +0300

    اكمال تهيئت المجريشن لجدول اولياء الامور واظافة الخصائص الاساسية له في ملف المودل لاوليائ الامور

---
+++ app/Models/Pareent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// use Illuminate\Notifications\Notifiable;
+
+class Pareent extends Model
+{
+    // use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',//'created_at',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
