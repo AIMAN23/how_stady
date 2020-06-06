208ef05184cfe62cb2e14a523d116d873ec52eaf
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-06-07 00:29:36 +0300

    ترتيب كود تصميم الشريط العلوي للصفحة الرئيسية للزوار الموقع

---
+++ resources/views/include/li.blade.php
@@ -0,0 +1,30 @@
+<!-- start items li for nav header and list mobile -->
+   
+        <li>
+            <a href="{{ route('welcome') }}">Home</a>
+        </li>
+        <li>
+            <a href="{{ route('welcome') }}">About Us</a>
+        </li>
+        <li>
+            <a href="{{ route('welcome') }}">Services</a>
+        </li>
+        <li>
+            <a href="#">Portfolio</a>
+            <ul class="dropdown">
+                <li><a href="{{ route('welcome') }}">Portfolio</a></li>
+                <li><a href="{{ route('welcome') }}">Single Portfolio</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">Blog</a>
+            <ul class="dropdown">
+                <li><a href="{{ route('welcome') }}">Blog List</a></li>
+                <li><a href="{{ route('welcome') }}">Single Blog</a></li>
+            </ul>
+
+        </li>
+        <li>
+            <a href="#contact-section">Contact</a>
+        </li>
+<!-- end items li for nav header and list mobile -->        
