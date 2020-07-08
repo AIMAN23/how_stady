6ef980cc16fbb43bb00ce7133c5c5ecff4610f3d
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-07-08 19:08:42 +0300

    add views agent

---
+++ resources/views/agent/home.blade.php
@@ -0,0 +1,252 @@
+@extends('layouts.agent')
+@section('main-header-li-left')
+    {{-- @include('admin.include.main-header-li-left') --}}
+@endsection
+
+@section('main-header-li-right')
+    {{-- @include('admin.include.main-header-li-right') --}}
+@endsection
+
+@section('sidebar-li')
+    {{-- @include('admin.include.sidebar-li') --}}
+        {{-- <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    {{ __('admin.Dashboard') }}
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('add.student', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.add student') }}</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('add.teacher', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.add teacher') }}</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('add.agent', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.add agent') }}</p>
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="nav-header">{{ __('admin.setting') }}</li>
+        <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    {{ __('admin.school setting') }}
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="{{ route('info.school', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.info school') }}</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('info.levels', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.levels') }}</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('info.classrooms', ['school_uuid'=>session('school.uuid') ,'admin_uuid'=>Auth::user()->uuid]) }}"
+                        class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>{{ __('admin.classrooms') }}</p>
+                    </a>
+                </li>
+            </ul>
+        </li> --}}
+@endsection
+
+@section('content-header')
+    <h1>{{ __('agent.Profile') }}</h1>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <!-- <div class="col-md-3">
+                    
+                </div> -->
+        <!-- /.col -->
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header p-2">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item"><a class="nav-link active" href="#Profile"
+                                data-toggle="tab">{{ __('agent.Profile') }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#About"
+                                data-toggle="tab">{{ __('agent.About Me') }}</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#settings"
+                                data-toggle="tab">{{ __('agent.setting') }}</a></li>
+                    </ul>
+                </div><!-- /.card-header -->
+                <div class="card-body">
+                    <div class="tab-content">
+
+                        <!-- 1 .tab-pane -->
+                        <div class="active tab-pane" id="Profile">
+                            <!-- Profile Image -->
+                            <div class="card card-primary card-outline">
+                                <div class="card-body box-profile">
+                                    <div class="text-center">
+                                        <img class="profile-user-img img-fluid img-circle"
+                                            {{-- src="{{ asset('img/.'.Auth::user()->image()->img) }}" alt="User profile picture"> --}}
+                                            src="{{ asset('images/user/'.App\Models\Image::find(3)->img) ?? '' }}" alt="User profile picture" />
+                                    </div>
+                                    {{-- <img src="{{ url('storage\العلاقات لقاعدة بيانات المكتبة.jpg') }}" width="100%" alt=""> --}}
+
+                                    {{-- <a href="{{ url('storage/csv/1592846022-students12.csv') }}">fil</a> --}}
+                                    <h3 class="profile-username text-center">{{ ('name') }}</h3>
+
+                                    <p class="text-muted text-center">{{ ('role') }}</p>
+
+                                    <!-- About Me Box -->
+                                    <div class="card card-primary">
+                                        <div class="card-header">
+                                            <h3 class="card-title">{{ __('admin.image') }}</h3>
+                                        </div>
+                                        <!-- /.card-header -->
+                                        <div class="card-body">
+                                            <strong><i class="fas fa-book mr-1"></i>{{ __('Education') }}</strong>
+
+                                            <p class="text-muted">
+                                                B.S. in Computer Science from the University of Tennessee at Knoxville
+                                            </p>
+
+                                            <hr>
+
+                                            <strong><i class="fas fa-map-marker-alt mr-1"></i>{{ __('Location') }}</strong>
+
+                                            <p class="text-muted">Malibu, California</p>
+
+                                            <hr>
+
+                                            <strong><i class="fas fa-pencil-alt mr-1"></i>{{ __('Skills') }}</strong>
+
+                                            <p class="text-muted">
+                                                <span class="tag tag-danger">UI Design</span>
+                                                <span class="tag tag-success">Coding</span>
+                                                <span class="tag tag-info">Javascript</span>
+                                                <span class="tag tag-warning">PHP</span>
+                                                <span class="tag tag-primary">Node.js</span>
+                                            </p>
+
+                                            <hr>
+
+                                            <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+
+                                            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing
+                                                elit. Etiam fermentum enim neque.</p>
+                                        </div>
+                                        <!-- /.card-body -->
+                                    </div>
+                                    <!-- /.card => About Me Box -->
+
+                                    <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- Profile Image /.card .card-primary .card-outline -->
+                        </div>
+                        <!-- 1/.tab-pane -->
+
+                        <!-- 2 .tab-pane -->
+                        <div class="tab-pane" id="About">
+                            {{--  --}}
+                        </div>
+                        <!-- 2/.tab-pane -->
+
+                        <!-- 3 .tab-pane -->
+                        <div class=" tab-pane" id="settings">
+                            <form class="form-horizontal">
+                                <div class="form-group row">
+                                    <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputName" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
+                                    <div class="col-sm-10">
+                                        <textarea class="form-control" id="inputExperience"
+                                            placeholder="Experience"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-sm-2 col-sm-10">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox"> I agree to the <a href="#">terms and
+                                                    conditions</a>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="offset-sm-2 col-sm-10">
+                                        <button type="submit" class="btn btn-danger">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        <!-- 3/.tab-pane -->
+
+                    </div>
+                    <!-- /.tab-content -->
+                </div><!-- /.card-body -->
+            </div>
+            <!-- /.nav-tabs-custom -->
+        </div>
+        <div class="col-md-3">
+            <ul>{{ "المراحل الدراسية" }}
+                @if (session('super.levels'))
+                @foreach (session('super.levels') as $level)
+
+                <li>{{ $level->name }}</li>
+                @endforeach
+                <li></li>
+
+                @endif
+            </ul>
+        </div>
+        <!-- /.col -->
+    </div>
+</div><!-- /.container-fluid -->
+@endsection
---
+++ resources/views/agent/setting/step_3.blade.php
@@ -0,0 +1,213 @@
+@extends('layouts.agent')
+@section('content')
+  <section class="content">
+    <div class="container col-md-8">
+      <div class="card card-widget widget-user">
+        <div class="widget-user-header bg-c text-white">
+            <h3 class="">{{('قم باضافة المشرفين')}}</h3>
+            <!-- <h5 class="widget-user-desc">{{('المرحلة')}} &amp; {{('الشعبة')}}</h5> -->
+          </div>
+
+
+        <div id="wrapper" class="clearfix">
+          <!-- preloader -->
+
+
+
+          <!-- Header -->
+
+
+
+
+
+
+
+
+
+
+
+
+          <div class="main-content">
+
+            <br>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <!--<div class="panel panel-default">-->
+                  <!--<div class="panel-heading">Register</div>-->
+
+                  <!--<div class="panel-body">-->
+
+                  <div class="row">
+                    <div class="col-md-8 col-xs-12 col-sm-6">
+                      <div class="alert alert-danger" style="display:none;">
+                        <a class="close" data-dismiss="alert" href="#" aria-hidden="true">×</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md-8 col-xs-12 col-sm-6">
+                      <div class="alert alert-success" style="display:none;">
+                        <a class="close" data-dismiss="alert" href="#" aria-hidden="true">×</a>
+                      </div>
+                    </div>
+                  </div>
+
+                  
+                  <br>
+                  {{-- <form method="POST" action="{{ route('agent.password.update') }}"> --}}
+                    @csrf
+                    {{--  --}}
+                    <div class="form-group row">
+                        <label for="supervisor_name" class="col-md-4 col-form-label text-md-right">اسم {{ __('lang.supervisor') }}</label>
+
+                        <div class="col-md-6">
+                            <input id="supervisor_name" type="text" class="form-control @error('supervisor_name') is-invalid @enderror" name="supervisor_name" value="{{ $supervisor_name ?? old('supervisor_name') }}" required autocomplete="supervisor_name" autofocus>
+
+                            @error('supervisor_name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+                    </div>
+                    {{--  --}}
+                    <div class="form-group row">
+                        <label for="levels" class="col-md-4 col-form-label text-md-right">{{ __('اختر المراحل الدراسية') }}</label>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                              <div class="form-check">
+                                <input id="l1" name="l1" class="form-check-input" type="checkbox">
+                                <label for="l1" class="form-check-label">Checkbox</label>
+                              </div>
+                              <div class="form-check">
+                                <input id="l2" name="l2" class="form-check-input" type="checkbox" checked="">
+                                <label for="l2" class="form-check-label">Checkbox checked</label>
+                              </div>
+                              <div class="form-check">
+                                <input id="l3" name="l3" class="form-check-input" type="checkbox" >
+                                <label for="l3" class="form-check-label">Checkbox disabled</label>
+                              </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('حفظ') }}
+                            </button>
+                        </div>
+                    </div>
+
+                  {{-- </form> --}}
+
+                </div>
+              </div>
+              <hr>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="card">
+                    <div class="card-header bg-c">
+                      <h3 class="card-title">{{ ('المراحل & المشرفين') }}</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                      <table class="table table-bordered">
+                        <thead>                  
+                          <tr>
+                            <th style="width: 10px">#</th>
+                            <th>المشرف</th>
+                            <th>المرحلة</th>
+                            <th style="width: 40px">الحالة</th>
+                            <th style="width: 40px">تعديل</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td>1.</td>
+                            <td>لايوجد</td>
+                            <td>الصف الاول الاساسي
+                              {{-- <div class="progress progress-xs">
+                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                              </div> --}}
+                            </td>
+                            <td><span class="badge bg-danger">لم يتم الاضافة بعد</span></td>
+                            <td><span class="badge bg-danger">تعديل & جديد</span></td>
+                          </tr>
+
+                          <tr>
+                            <td>1.</td>
+                            <td>لايوجد</td>
+                            <td>الصف الثاني الاساسي
+                              {{-- <div class="progress progress-xs">
+                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                              </div> --}}
+                            </td>
+                            <td><span class="badge bg-danger">لم يتم الاضافة بعد</span></td>
+                            <td><span class="badge bg-danger">تعديل & جديد</span></td>
+                          </tr>
+                          
+                          <tr>
+                            <td>1.</td>
+                            <td>لايوجد</td>
+                            <td>الصف الثالث الاساسي
+                              {{-- <div class="progress progress-xs">
+                                <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                              </div> --}}
+                            </td>
+                            <td><span class="badge bg-danger">لم يتم الاضافة بعد</span></td>
+                            <td><span class="badge bg-danger">تعديل & جديد</span></td>
+                          </tr>
+
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.card-body -->
+                    {{-- <div class="card-footer clearfix">
+                      <ul class="pagination pagination-sm m-0 float-right">
+                        <li class="page-item"><a class="page-link" href="#">«</a></li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item"><a class="page-link" href="#">»</a></li>
+                      </ul>
+                    </div> --}}
+                  </div>
+                </div>
+              </div>
+            </div> <!-- END of Container -->
+            <!-- Footer -->
+
+
+
+
+          </div>
+          <!--POPUP MODAL-->
+         
+
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+@endsection
+
+@section('ajax')
+<script>
+  function get_code(cur) {
+    if (cur.value != '') {
+      var option = $('option:selected', cur).attr('code');
+      $('#country_code').val(option);
+      $('.country_code').html(option);
+    } else
+      $('.country_code').html('');
+    //alert(option);    
+  }
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+</script>
+@endsection
