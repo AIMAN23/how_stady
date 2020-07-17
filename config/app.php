5e6e5b93c0db8b99dfb25dd1e86ef894002a708d
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-07-17 05:18:28 +0300

    تصحيح الخطاء عند رفع الموقع

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         // Illuminate\Html\HtmlServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
         /*
          * Package Service Providers...
          */
@@ -228,10 +227,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'LaravelLocalization' => Mcamara\LaravelLocalization\Facades\LaravelLocalization::class,
-        // 'Form' => Illuminate\Html\FormFacades\View::class,
-        // 'Html' => Illuminate\Html\HtmlFacades\View::class,
-        'Form' => Illuminate\Html\FormFacade::class,
-        'Html' => Illuminate\Html\HtmlFacade::class,
 
     ],
 
