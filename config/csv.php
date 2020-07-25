acae9a1f5f9f97b7db74170fe87f304c97562b30
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-07-25 04:51:54 +0300

    انشاء ملف الكنغجريشن للملفات الاكسل

---
+++ config/csv.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'student'=>[
+        
+        'keys'=>[
+             'no_student',
+             'stu_name',
+             'level_no',
+             'classroom_name',
+             'pareent_name',
+             'pareent_mobile',
+             'stu_gender'
+         ],
+    ],
+   
+];
+//  كود الربط مع mailtrap التجريبي لارسال الايميلات
+// return [
+//     "driver" => "smtp",
+//     "host" => "smtp.mailtrap.io",
+//     "port" => 2525,
+//     "from" => array(
+//         "address" => "from@example.com",
+//         "name" => "Example"
+//     ),
+//     "username" => "8401e580283efe",
+//     "password" => "5961e6f64b8da6",
+//     "sendmail" => "/usr/sbin/sendmail -bs"
+//   ];
