743587fd0c8a654ccb7bc1cb4cf47314ed525a7c
AIMAN23 <59153221+AIMAN23@users.noreply.github.com>
2020-08-02 03:36:42 +0300

    create('schools_teachers') table

---
+++ database/migrations/2020_08_02_022934_schools_teachers.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SchoolsTeachers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schools_teachers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // Many to Many
+            $table->bigInteger('school_id')->unsigned()->default(0)->comment('');
+            $table->bigInteger('teacher_id')->unsigned()->default(0);
+            
+            
+            // One to Many
+            $table->bigInteger('level_id')->unsigned()->default(0)->comment('');
+            $table->bigInteger('subjcte_id')->unsigned()->default(0)->comment('');
+            // 
+            $table->string('code')->nullable()->comment('كود تفعيل حساب المعلم في المدرسة');
+            $table->tinyInteger('status')->nullable()->default(0)->comment('حالة تفعيل الحساب');
+            // 
+            $table->foreign('level_id')->references('id')->on('levels')->onDelete('cascade');
+            $table->foreign('subjcte_id')->references('id')->on('subjctes')->onDelete('cascade');
+            // 
+            $table->timestamp('added_on')->nullable()->default(time());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schools_teachers');
+    }
+}
